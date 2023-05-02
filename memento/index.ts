5d83722842c8b9d43c9489f26ee6fc4d04994347
liucuxiu <xliucuxiux@gmail.com>
2023-05-02 13:05:22 +0700

    memento pattern

--- memento/index.ts
+++ memento/index.ts
@@ -8,7 +8,7 @@ const history = new History()
 editor.content = 'a'
 history.push(editor.createState())
 
-editor.content = 'b1'
+editor.content = 'b'
 history.push(editor.createState())
 
 editor.content = 'c'
